2d200349aaf7509148d73dc33b49c238d692ae13
Nathan Sullivan <nsullivan@netsuite.com>
2021-01-17 04:20:47 -0800

    Solution for Day 1 Part 2
    Replace ad-hoc pair generation with a more general combinatoric
    function, including switching the Part 1 solution to using it.

--- src/advent/day1.clj
+++ src/advent/day1.clj
@@ -12,19 +12,34 @@
 
 (def target-sum 2020)
 
-(defn find-target-pair
-  [coll target]
-  (let [pairs (map vector (repeat (first coll)) (rest coll))
-        found-target (filter #(= target (apply + %)) pairs)]
-    (if (empty? found-target)
-      (find-target-pair (rest coll) target)
-      (first found-target))))
+(defn combinations
+  [m coll]
+  (letfn [(comb-aux
+            [m drop-num]
+            (if (= 1 m)
+              (for [x (drop drop-num coll)]
+                (list x))
+              (for [c (range drop-num (count coll))
+                    xs (comb-aux (dec m) (inc c))]
+                (cons (nth coll c) xs))))]
+    (comb-aux m 0)))
+
+(defn find-target-combo
+  [coll size target]
+  (let [candidates (combinations size coll)
+        found-target (filter #(= target (apply + %)) candidates)]
+    (first found-target)))
 
 (defn process-expense-report
-  [report target]
-  (apply * (find-target-pair report target)))
+  [report combo-size target]
+  (apply * (find-target-combo report combo-size target)))
 
 (defn part1
   []
   (let [report (map edn/read-string (slurp-lines "input1.txt"))]
-    (process-expense-report report target-sum)))
+    (process-expense-report report 2 target-sum)))
+
+(defn part2
+  []
+  (let [report (map edn/read-string (slurp-lines "input1.txt"))]
+    (process-expense-report report 3 target-sum)))
