11550c49c63a7aa4f458dbe8ce2c4b244428f0f3
Nathan Sullivan <nsullivan@netsuite.com>
2021-10-28 19:43:29 -0700

    Add solution for Day 8 Part 1

--- src/advent/day8.clj
+++ src/advent/day8.clj
@@ -1,5 +1,7 @@
 (ns advent.day8
-  (:require [advent.util :refer [slurp-lines]]))
+  (:require [advent.util :refer [slurp-lines]]
+            [clojure.string :as string]
+            [clojure.edn :as edn]))
 
 (def test-input
   ["nop +0"
@@ -11,3 +13,32 @@
    "acc +1"
    "jmp -4"
    "acc +6"])
+
+(defn parse-instruction
+  [instruction]
+  (let [[opname arg] (string/split instruction #" ")]
+    [(keyword opname) (edn/read-string arg)]))
+
+(defn make-computer
+  [code]
+  {:ip 0 :code code :acc 0})
+
+(defn process-instruction
+  [computer]
+  (let [[opcode arg] (nth (computer :code) (computer :ip))]
+    (case opcode
+      :nop (update computer :ip inc)
+      :jmp (update computer :ip + arg)
+      :acc (update (update computer :ip inc) :acc + arg))))
+
+(defn run-computer
+  [computer visited]
+  (if (contains? visited (computer :ip))
+    computer
+    (recur (process-instruction computer) (conj visited (computer :ip)))))
+
+(defn part1
+  []
+  (let [code (map parse-instruction (slurp-lines "input8.txt"))
+        computer (make-computer code)]
+    (:acc (run-computer computer #{}))))
