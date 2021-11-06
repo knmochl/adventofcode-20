68a5c90f92dc7116103cf258f71b914ebd0e68a5
Nathan Sullivan <nsullivan@netsuite.com>
2021-11-05 21:08:11 -0700

    Add solution for Day 9 Part 1

--- src/advent/day9.clj
+++ src/advent/day9.clj
@@ -5,3 +5,33 @@
 
 (def test-input
   [35 20 15 25 47 40 62 55 65 95 102 117 150 182 127 219 299 277 309 576])
+
+(defn rolling-window
+  [coll size start]
+  (take size
+        (drop start coll)))
+
+(defn pair-combinations
+  [coll]
+  (if (empty? coll) []
+      (concat
+       (map vector (repeat (first coll)) (drop 1 coll))
+       (pair-combinations (drop 1 coll)))))
+
+(defn check-xmas
+  [value preamble]
+  (some #{value} (map (partial apply +) (pair-combinations preamble))))
+
+(defn find-first-invalid
+  [coll preamble-size]
+  (when (> (count coll) preamble-size)
+    (let [value (first (drop preamble-size coll))
+          preamble (take preamble-size coll)]
+      (if (check-xmas value preamble)
+        (find-first-invalid (drop 1 coll) preamble-size)
+        value))))
+
+(defn part1
+  []
+  (let [codes (map edn/read-string (slurp-lines "input9.txt"))]
+    (find-first-invalid codes 25)))
