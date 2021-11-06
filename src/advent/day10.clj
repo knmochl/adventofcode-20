91a069962aa921e36120288836f8f408cab5cc82
Nathan Sullivan <nsullivan@netsuite.com>
2021-11-05 21:15:58 -0700

    Add test-input for Day 10

---
+++ src/advent/day10.clj
@@ -0,0 +1,6 @@
+(ns advent.day10
+  (:require [advent.util :refer [slurp-lines]]
+            [clojure.string :as string]
+            [clojure.edn :as edn]))
+
+(def test-input [28 33 18 42 31 14 46 20 48 47 24 23 49 45 19 38 39 11 1 32 25 35 8 17 7 9 4 2 34 10 3])
